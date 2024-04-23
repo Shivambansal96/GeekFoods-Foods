2a42dbf123c1e52e415458ba4367e2fec92d1bcc
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-04-23 19:55:37 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import { useState } from "react";
 import "./App.css";
-import Nav from "./components/Nav.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/screens/LandingPage.jsx";
 import QuoteScreen from "./components/screens/QuoteScreen.jsx";
+import RestaurantPage from "./components/screens/RestaurantPage.jsx";
+import FoodsPage from "./components/screens/FoodsPage.jsx";
+import ContactPage from "./components/screens/ContactPage.jsx";
 
 function App() {
 
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   // function which will get a ID
 
   return (
     
-    <div className="App">
-     {/* {
-      page == 0?<LandingPage />:(page == 1?<QuoteScreen />:null)
-     }  */}
+    // <div className="App">
+      
+    //   <LandingPage />
+    //   <QuoteScreen />
 
-     <Nav />
-     
+    //  <RestaurantPage />
 
-    </div>  
+    // </div>  
     
+
+
+    <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/quote" element={<QuoteScreen/>} />
+        <Route path="/restaurant" element={<RestaurantPage/>} />
+        <Route path="/foods" element={<FoodsPage/>} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import "./Nav.css"
-import "./assets/logo.svg";
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav(props) { 
     return (
@@ -10,15 +11,25 @@ function Nav(props) {
                 <h1>GeekFoods</h1>
             </div>
             
-            <div id="mid">
+            {/* <div id="mid">
                     
                 <div id="home"><a id="home2" href={props.pageNumber}>Home</a></div>  
-                {/* isko anchor tag se hatana parega warna page refresh hoga and ispe onlick lagana parega onClick={function} */}
                 <div id="quote"><a id="quote2" href={props.pageNumber}>Quote</a></div>
                 <div id="restaurant"><a id="restaurant2" href="#">Restaurant</a></div>
                 <div id="foods"><a id="foods2" href="#">Foods</a></div>
                 <div id="contact"><a id="contact2" href="#">Contact</a></div>
+                <div id="home"><Link to='/'>Home</Link></div>
+                <div id="quote"><Link to='/quote'>Quote</Link></div>
+                <div id="restaurant"><Link to='/restaurant'>Restaurant</Link></div>
 
+            </div>   */}
+
+            <div id="mid">
+                <div id="home"><NavLink to='/' activeClassName="active">Home</NavLink></div>
+                <div id="quote"><NavLink to='/quote' activeClassName="active">Quote</NavLink></div>
+                <div id="restaurant"><NavLink to='/restaurant' activeClassName="active">Restaurant</NavLink></div>
+                <div id="foods"><NavLink to='/foods' activeClassName="active">Foods</NavLink></div>
+                <div id="contact"><NavLink to='/contact' activeClassName="active">Contact</NavLink></div>
             </div>
             
             <div id="right">
@@ -29,10 +40,53 @@ function Nav(props) {
             </div>
 
             <div id="menu-icon-phone-view">
-                <i className="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-bars"></i>
             </div>
         </div>
     );
 }
 
 export default Nav;
+
+
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import './Nav.css';
+
+// function Nav(props) { 
+//     return (
+//         <div id="navbar">
+//             <div id="left">
+//                 <img src="./assets/logo.svg" alt="GeekFoods Logo"/>
+//                 <h1>GeekFoods</h1>
+//             </div>
+            
+//             <div id="mid">
+//                 <div id="home">
+//                     <NavLink to="/" activeClassName="active">Home</NavLink>
+//                 </div>  
+//                 <div id="quote">
+//                     <NavLink to="/quote" activeClassName="active">Quote</NavLink>
+//                 </div>
+//                 <div id="restaurant">
+//                     <NavLink to="/restaurant" activeClassName="active">Restaurant</NavLink>
+//                 </div>
+//                 <div id="foods"><a id="foods2" href="#">Foods</a></div>
+//                 <div id="contact"><a id="contact2" href="#">Contact</a></div>
+//             </div>  
+
+//             <div id="right">
+//                 <div id="button-container">
+//                     <button type="submit">Get Started</button>
+//                 </div>
+//             </div>
+
+//             <div id="menu-icon-phone-view">
+//                 <i className="fa-solid fa-bars"></i>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Nav;
